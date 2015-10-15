dee2b7a4bdd19af42c8f66d683291064fc9d330a
szow79 <szow79@gmail.com>
2015-10-15 11:54:46 -0700

    add random button

--- public/js/application.js
+++ public/js/application.js
@@ -7,4 +7,16 @@ $(document).ready(function () {
     request.done(function () { window.location = "/"; });
   });
 
+  $('#fun-btn').on('click', function(e){
+  	e.preventDefault()
+
+  	var random = Math.floor(Math.random() * 6)
+  	if (random === 0) $('#fun-btn').animate({'right': '-=50px'}, 'fast')
+  	if (random === 1) $('#fun-btn').animate({'right': '+=50px'}, 'fast')
+  	if (random === 2) $('#fun-btn').fadeOut('slow', function(){})
+  	if (random === 3) $('#fun-btn button').animate({'height': '20px', 'width': '20px'})
+  	if (random === 4) $('#fun-btn button').animate({'height': '50px', 'width': '50px'})
+  	if (random === 5) $('#fun-btn button').animate({'height': '60px', 'width': '20px'})
+  }) 
+
 });
